1c836d33b5de8d8923a5812e8e2cc05d66453458
Nitin Yadav <ydvnitin16@gmail.com>
2025-11-07 21:29:45 +0530

    refactor: changed in-progress bagde color to green

--- app/issues/[id]/page.js
+++ app/issues/[id]/page.js
@@ -32,7 +32,7 @@ const page = async ({ params }) => {
                                      issue.status === 'open'
                                          ? 'bg-red-500/20 text-red-700 border border-red-500/30 dark:bg-red-500/30 dark:text-red-300 dark:border-red-500/50'
                                          : issue.status === 'in progress'
-                                         ? 'bg-yellow-500/20 text-yellow-700 border border-yellow-500/30 dark:bg-yellow-500/30 dark:text-yellow-200 dark:border-yellow-500/50'
+                                         ? 'bg-green-500/20 text-green-700 border border-green-500/30 dark:bg-green-500/30 dark:text-green-300 dark:border-green-500/50'
                                          : issue.status === 'closed'
                                          ? 'bg-purple-500/20 text-purple-700 border border-purple-500/30 dark:bg-purple-500/30 dark:text-purple-200 dark:border-purple-500/50'
                                          : 'bg-zinc-500/20 text-zinc-700 border border-zinc-500/30 dark:bg-zinc-700/30 dark:text-zinc-300 dark:border-zinc-600/50'
