8869a44e9f4f9d5c7a709feff08a50dd175986a5
Nitin Yadav <ydvnitin16@gmail.com>
2025-11-09 22:22:21 +0530

    Feat: Add PUT endpoint for issue

--- app/api/issues/[id]/route.js
+++ app/api/issues/[id]/route.js
@@ -16,3 +16,30 @@ export async function GET(request, { params }) {
         { status: 200 }
     );
 }
+
+export async function PUT(request, { params }) {
+    const { id } = await params;
+    const body = await request.json();
+    if (!body || !body.title || body.title.trim() === '') {
+        return NextResponse.json(
+            { success: false, error: 'Title is empty' },
+            { status: 400 }
+        );
+    }
+    const issue = await prisma.issue.findUnique({ where: { id: id } });
+
+    if (!issue)
+        return NextResponse.json(
+            { success: false, error: 'Issue Not Found' },
+            { status: 404 }
+        );
+
+    const updatedIssue = await prisma.issue.update({
+        where: { id },
+        data: { title: body.title, description: body.description },
+    });
+    return NextResponse.json(
+        { success: true, message: 'Issue updated successfully', updatedIssue },
+        { status: 200 }
+    );
+}
