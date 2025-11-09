609d3b93b4a679718484392648c76a33585cc312
Nitin Yadav <ydvnitin16@gmail.com>
2025-11-09 14:45:32 +0530

    Refactor: more better response from server

--- app/api/issues/[id]/route.js
+++ app/api/issues/[id]/route.js
@@ -3,13 +3,16 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request, { params }) {
     const { id } = await params;
-    const issue = await prisma.issue.findUnique({where: {id: id}})
-    console.log('Issue: ',issue)
+    const issue = await prisma.issue.findUnique({ where: { id: id } });
+
     if (!issue)
         return NextResponse.json(
             { message: 'Issue Not Found' },
             { status: 404 }
         );
 
-    return NextResponse.json(issue, { status: 200 });
+    return NextResponse.json(
+        { succes: true, message: 'Issue fetched successfully', issue },
+        { status: 200 }
+    );
 }
--- app/api/issues/route.js
+++ app/api/issues/route.js
@@ -4,7 +4,10 @@ import { NextResponse } from 'next/server';
 export async function GET() {
     try {
         const issues = await prisma.issue.findMany();
-        return NextResponse.json(issues);
+        return NextResponse.json(
+            { success: true, message: 'Issues fetched successfully', issues },
+            { status: 200 }
+        );
     } catch (err) {
         return NextResponse.json({ error: err.message }, { status: 500 });
     }
@@ -13,22 +16,25 @@ export async function GET() {
 export async function POST(request) {
     try {
         const body = await request.json();
-        if (!body || !body.title) {
+        if (!body || !body.title || body.title.trim() === '') {
             return NextResponse.json(
                 { error: 'Missing title' },
                 { status: 400 }
             );
         }
 
-        const created = await prisma.issue.create({
+        const issue = await prisma.issue.create({
             data: {
                 title: body.title,
                 description: body.description || '',
-                status: 'open'
+                status: 'open',
             },
         });
 
-        return NextResponse.json(created, { status: 201 });
+        return NextResponse.json(
+            { success: true, message: 'Issue created successfully.', issue },
+            { status: 201 }
+        );
     } catch (err) {
         return NextResponse.json({ error: err.message }, { status: 500 });
     }
