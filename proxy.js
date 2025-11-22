de4f707307ea8edfae5c943f7c6e88a0fed11792
Nitin Yadav <ydvnitin16@gmail.com>
2025-11-22 14:48:16 +0530

    Improved proxy for the pages

--- proxy.js
+++ proxy.js
@@ -2,11 +2,25 @@ import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from './app/auth/authOptions';
 
+const publicPaths = ['/auth'];
+
+const isPublicPath = (pathname) => {
+    return publicPaths.some((path) => pathname.startsWith(path));
+};
+
 export async function proxy(request) {
     const session = await getServerSession(authOptions);
     const pathname = request.nextUrl.pathname;
 
-    if (!session && !pathname.startsWith('/auth')) {
+    if (pathname.startsWith('/api')) {
+        return NextResponse.next();
+    }
+
+    if (isPublicPath(pathname)) {
+        return NextResponse.next();
+    }
+
+    if (!session) {
         const url = request.nextUrl.clone();
         url.pathname = '/auth/signin';
         return NextResponse.redirect(url);
