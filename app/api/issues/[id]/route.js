39b20778c5e21200e051fc5841f33e013d765ba2
Nitin Yadav <ydvnitin16@gmail.com>
2025-11-24 14:22:37 +0530

    Feat: Add role based control to edit issue admin, issue asssigned to user, issue created by user can edit issue

--- app/api/issues/[id]/route.js
+++ app/api/issues/[id]/route.js
@@ -1,4 +1,6 @@
+import { authOptions } from '@/app/auth/authOptions';
 import { prisma } from '@/lib/prisma';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 
 export async function GET(request, { params }) {
@@ -24,6 +26,43 @@ export async function GET(request, { params }) {
 
 export async function PUT(request, { params }) {
     const { id } = await params;
+    const { user } = await getServerSession(authOptions);
+
+    // check is authorized
+    if (!user)
+        return NextResponse.json(
+            { success: false, error: 'Not authorized' },
+            { status: 401 }
+        );
+
+    // is exists in the db
+    const dbUser = await prisma.user.findUnique({ where: { id: user.id } });
+    if (!dbUser)
+        return NextResponse.json(
+            { success: false, error: 'User not found' },
+            { status: 404 }
+        );
+
+    // is issue exists
+    const issue = await prisma.issue.findUnique({ where: { id: id } });
+    if (!issue)
+        return NextResponse.json(
+            { success: false, error: 'Issue Not Found' },
+            { status: 404 }
+        );
+
+    // is he authorized to perform this action
+    if (
+        dbUser.role !== 'ADMIN' &&
+        issue.createdById !== dbUser.id &&
+        issue.assignedUserId !== dbUser.id
+    ) {
+        return NextResponse.json(
+            { success: false, error: 'You are not allowed for this action' },
+            { status: 405 }
+        );
+    }
+
     const body = await request.json();
     if (!body || !body.title || body.title.trim() === '') {
         return NextResponse.json(
@@ -31,13 +70,6 @@ export async function PUT(request, { params }) {
             { status: 400 }
         );
     }
-    const issue = await prisma.issue.findUnique({ where: { id: id } });
-
-    if (!issue)
-        return NextResponse.json(
-            { success: false, error: 'Issue Not Found' },
-            { status: 404 }
-        );
 
     const updatedIssue = await prisma.issue.update({
         where: { id },
